9ab58212aa224459d0071221c50c1c13ca752df1
严淑刚 <stavyan@yanshugangdeMacBook-Pro.local>
2020-07-01 16:19:22 +0800

    项目初始化

---
+++ App.vue
@@ -0,0 +1,20 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+  /*#ifndef MP*/
+  @import url('https://at.alicdn.com/t/font_1681579_yaiyfblc2xt.css');
+  /*#endif*/
+</style>
